25b7503feb13de575ec7960a03234157e7b31f58
Jaco Erasmus <jierasmus27@gmail.com>
2015-06-27 15:46:12 +0200

    Added item scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150627134416) do
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.text     "description", limit: 65535
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
 end
