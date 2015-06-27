9c3b29ad45c157380a709d131548ab15767d8404
Jaco Erasmus <jierasmus27@gmail.com>
2015-06-27 16:06:55 +0200

    Add categories additional to items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627134416) do
+ActiveRecord::Schema.define(version: 20150627134807) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  create_table "categories_items", id: false, force: :cascade do |t|
+    t.integer "category_id", limit: 4
+    t.integer "item_id",     limit: 4
+  end
+
+  add_index "categories_items", ["category_id"], name: "index_categories_items_on_category_id", using: :btree
+  add_index "categories_items", ["item_id"], name: "index_categories_items_on_item_id", using: :btree
 
   create_table "items", force: :cascade do |t|
     t.string   "name",        limit: 255
